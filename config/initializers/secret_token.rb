9bd3ccf486f925675992b6ad14f4daff5e435104
arcoiris <deborlora@gmail.com>
2013-12-11 17:27:35 -0500

    Initial commit along with a basic homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ConnectingLearners::Application.config.secret_key_base = '031b32e8b71d67a3503162e808fef392a5f16e090263c15dfef9ad39922cc0338f2cbf0f2c953d218f5984a9b475241b3bca560bcf2d5412472754ee8c2fea38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ConnectingLearners::Application.config.session_store :cookie_store, key: '_connecting_learners_session'
