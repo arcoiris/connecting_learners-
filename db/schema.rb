a6397225e03682655e46c6104dc12b87439b3895
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2013-12-21 16:45:07 -0500

    Create a topic table with a topic field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131221213154) do
+ActiveRecord::Schema.define(version: 20131221213454) do
+
+  create_table "topics", force: true do |t|
+    t.string   "topic"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
