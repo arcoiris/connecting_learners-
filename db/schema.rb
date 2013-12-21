c52b5f8decd36cbc33a845dc97fc073342737238
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2013-12-21 16:33:14 -0500

    Delete another incorrect table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216004239) do
+ActiveRecord::Schema.define(version: 20131221213154) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
