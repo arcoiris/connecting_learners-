26cd547634ccc4fe158b735af8fa670a0a51f9ba
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2013-12-22 15:27:40 -0500

    Create a subtopics table which belongs_to the topics table (has_many)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131221215637) do
+ActiveRecord::Schema.define(version: 20131222202205) do
+
+  create_table "subtopics", force: true do |t|
+    t.string   "name"
+    t.integer  "topic_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "topics", force: true do |t|
     t.string   "name"
