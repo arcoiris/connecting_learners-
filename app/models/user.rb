1775620847e71e4797398c5280d52ea4ea6741b9
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2013-12-30 19:56:22 -0500

    Add validations to the geocode zip code that is to be entered when signing up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   geocoded_by :zip_code
+  validates :zip_code, presence: :true
+  validates :zip_code, :numericality => { :only_integer => true }
+  validates :zip_code, :length => { :is => 5 }
   after_validation :geocode, :if => :zip_code_changed?
 end
