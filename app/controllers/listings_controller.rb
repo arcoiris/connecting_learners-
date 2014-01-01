5379768a9f5d582cc28eb89080027001e87fd293
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2013-12-31 19:09:34 -0500

    Listings new view

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,6 +5,21 @@ class ListingsController < ApplicationController
 
   def show
   	@listing = Listing.find params[:id]
-  	@user = User.find params [:id]
+  	@user = current_user
   end
+
+  def new 
+  	@listing = Listing.new
+    @subtopics = Subtopic.all
+  end
+
+  def create
+    if user_signed_in? 
+      safe_listing = params.require(:listing).permit(:title, :description, :subtopic_id).merge(user_id: current_user.id)
+      @listing = Listing.create safe_listing
+      redirect_to @listing
+    else
+      redirect_to new_user_session_path, alert: "Only logged in users can create listings." 
+    end
+  end 
 end
