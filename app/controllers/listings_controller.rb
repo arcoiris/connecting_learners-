28b1f3f5bb33683823c952d3018fca0f4375a9fd
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2014-01-10 17:57:54 -0500

    Adding edit feature to user, with precautions for non-posters to edit others' listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -22,5 +22,25 @@ class ListingsController < ApplicationController
     else
       redirect_to new_user_session_path, alert: "Only logged in users can create listings." 
     end
-  end 
+  end
+
+  def edit
+    @listing = Listing.find params[:id]
+    @subtopics = Subtopic.all
+    @user_zip_code = current_user.zip_code
+    if current_user != @listing.user
+      flash[:alert] = "You are not authorized to execute this action"
+      redirect_to root_path
+    end
+  end
+
+  def update
+      @listing = Listing.find params[:id]
+      safe_listing = params.require(:listing).permit(:title, :description, :subtopic_id, :virtual, :in_person, :address, :latitude, :longitude).merge(user_id: current_user.id)
+      @listing.update safe_listing
+      redirect_to @listing
+  end
+
+  def destroy
+  end
 end
