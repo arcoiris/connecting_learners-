e267ac66a2eaf73d4c968d630073f565298dde25
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2014-01-09 21:56:30 -0500

    Deleting crappy migrations

--- db/migrate/20140105191030_drop_availabilities_and_days_tables.rb
+++
@@ -1,7 +0,0 @@
-class DropAvailabilitiesAndDaysTables < ActiveRecord::Migration
-# no longer using associations to handle availabilities
-  def change
-  	drop_table :days
-  	drop_table :availabilities
-  end
-end
--- db/migrate/20140105212506_add_listing_reference_to_availabilities.rb
+++
@@ -1,5 +0,0 @@
-class AddListingReferenceToAvailabilities < ActiveRecord::Migration
-  def change
-    add_column :availabilities, :listing_id, :integer
-  end
-end
